fd2141d3a3a3882f524ae9e346f10aa37e7d68f5
decuma02 <108753137+decuma02@users.noreply.github.com>
2026-07-14 15:54:33 +0530

    feat: init setup for the project

---
+++ src/components/MovieGridCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieGridCard = () => {
+  return (
+    <div>MovieGridCard</div>
+  )
+}
+
+export default MovieGridCard
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import {} from 'react'
+
+const Home = () => {
+  return (
+    <>
+        <div class="page-top"></div>
+        <input class="search-bar"></input>
+        <div>Home</div>
+    </>
+  )
+}
+
+export default Home
